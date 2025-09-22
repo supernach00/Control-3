8625eb5a123f408145667eab421c848d1527cf57
nachito <igchantiri99@gmail.com>
2025-09-22 13:52:33 -0300

    tp3 inicio

---
+++ Ejercicios/TP1/MatlabTP1.m
@@ -0,0 +1,28 @@
+%% Ejercicio 4 c
+s = tf('s');
+
+% Familia de plantas
+
+g = 1/(s+1);
+
+% Planta nominal
+
+g0 = 1/(s+1);
+g0.IOdelay = 12.5;
+
+
+% Parametros para graficar 
+n = 1e3;
+w = logspace(1,2, n);
+L = linspace(10, 15, n);
+
+% Planta
+
+respuestag_0 = squeeze(freqresp(g0,w));
+
+for i = 1:n
+    g.IOdelay = L(i);
+end
+    
+
+     
